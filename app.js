d22e3435fb7804cabcfe26f40648a1a34bf69365
oliviakim217 <oliviakim217@users.noreply.github.com>
2022-03-22 22:22:28 -0400

    Add the work route

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Root Route
 app.get("/", function (req, res) {
     let today = new Date();
     let options = {
@@ -18,16 +20,35 @@ app.get("/", function (req, res) {
     let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", { 
-        kindOfDay: day,
+        listTitle: day,
         newListItems: items
     });
 });
 
 app.post("/", function(req, res) {
+    console.log(req.body);
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+    
+    
 });
+
+// Work Route
+app.get("/work", function(req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
+});
+
+
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.")
