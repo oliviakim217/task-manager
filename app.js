30741c1631d48cd5b48263de695ac381a1b80514
oliviakim217 <oliviakim217@users.noreply.github.com>
2022-04-06 11:46:42 -0400

    Delete the date module and create a todolistDB with default items.

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 
 const app = express();
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Create DB - CHANGE LATER
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome To My To-do List!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully inserted default items.")
+    }
+});
+
 // Root Route
 app.get("/", function(req, res) {
-    const day = date.getDate();
-
     res.render("list", { 
-        listTitle: day,
+        listTitle: "Today",
         newListItems: items
     });
 });
